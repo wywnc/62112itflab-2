41a7d5e9cafd2c5589c17c76095463d5c0d2bb45
wywnc <73680478+wywnc@users.noreply.github.com>
2020-11-06 18:57:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,25 +10,18 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col">
                 <div class="card">
                         <img class="card-img-top" src="office1.jpg">
                     <div class="card-body">
                         <p>Lorem ipsum dolor, sit amet consectetur!</p>
                     </div>
                 </div>
-            </div>
-            <div class="col">
-        <div class="card">
-                    <img class="card-img-top" src="img/staffs/staff4.jpg" >
+                <div class="card">
+                        <img class="card-img-top" src="office1.jpg">
                     <div class="card-body">
-                        <h4 class="card-title">Our Staff</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur !<p>
-                    <a href="#" class="btn btn-primary">Read more</a>
+                        <p>Lorem ipsum dolor, sit amet consectetur!</p>
                     </div>
                 </div>
-            </div>
-            
         </div>
     </div>
 </body>
