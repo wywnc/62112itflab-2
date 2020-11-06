74984830b274dffa806f126bd306b8f83bf20427
wywnc <73680478+wywnc@users.noreply.github.com>
2020-11-06 19:00:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
                 <div class="card">
                         <img class="card-img-top" src="office1.jpg">
