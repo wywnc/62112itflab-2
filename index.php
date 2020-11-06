3462d63a1a18e3c204b9cad414d719718c7f7afa
wywnc <73680478+wywnc@users.noreply.github.com>
2020-11-06 18:34:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,25 +12,67 @@
         <div class="row">
             <div class="col">
                 <div class="card">
-                    <div class="card-header" >
-                        <img src="office1.jpg">
+                    <div class="col-sm-6">
+                        <img class="card-img-top" 
+                        src="img/work1.jpg" 
+                        style="height: 90%; width: 100%"
+                        class="img-fluid">
+                    </div>
+                    <div class="card-header bg-danger text-white" >
+                        <h4>Hot news</h4>
                     </div>
                     <div class="card-body">
-                        <p>Lorem ipsum dolor, sit amet consectetur!</p>
+                        <h4 class="card-title">Telephone</h4>
+                        <p class="card-text">New Iphone12<p>
+                    <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                    <div class="card-body">
+                        <p style="color: rgb(192, 175, 45);"> New Telephone </p>
                     </div>
                 </div>
             </div>
             <div class="col">
         <div class="card">
-                    <img class="card-img-top" src="office10.jpg" >
+                    <div class="col-sm-8">
+                    <img class="card-img-top" 
+                    src="img/office5.jpg"
+                    style="height: 90%; width: 100%"
+                    class="img-fluid">
+                    </div>
+                    <div class="card-header bg-danger text-white" >
+                        <h4>Hot news</h4>
+                    </div>
                     <div class="card-body">
-                        <h4 class="card-title">Our Staff</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur !<p>
-                    <a href="#" class="btn btn-primary">Read more</a>
+                        <h4 class="card-title">Today Weather</h4>
+                        <p class="card-text">Thailand now in summer<p>
+                    <a href="#" class="btn btn-primary">Summer in Thailand</a>
+                    </div>
+                    <div class="card-body">
+                        <p style="color: rgb(192, 175, 45);"> Lorem ipsum dolor, sit amet consectetur! </p>
                     </div>
                 </div>
             </div>
-            
+            <div class="col">
+        <div class="card">
+                    <div class="col-sm-30">
+                    <img class="card-img-top" 
+                    src="img/banner1.jpg"
+                    style="height: 90%; width: 100%"
+                    class="img-fluid">
+                    </div>
+                    <div class="card-header bg-danger text-white" >
+                        <h4>Hot news</h4>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title">education</h4>
+                        <p class="card-text">Thailand education<p>
+                    <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                    <div class="card-body">
+                        <p style="color: rgb(192, 175, 45);"> Thai student </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
